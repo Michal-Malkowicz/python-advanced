26c6eeecb3c9ef902c1a50b084a2e394c40dd1a3
Michal Malkowicz <michal.malkowicz@diehl.com>
2021-06-01 09:31:28 +0200

    parser done

--- 2-parsing/parser.py
+++ 2-parsing/parser.py
@@ -1,3 +1,6 @@
+import re
+
+
 # a = [1, 2, 3]
 # a = [str(element) for element in a]
 #
@@ -57,10 +60,73 @@ def validate(passports: list) -> list:
     return result
 
 
-passports = agregate_people('input.txt')
-wrong_ids = validate(passports)
-print(f'Passports:\n {len(passports)}\nWrong ids:\n {len(wrong_ids)}\nList of wrong ids:\n{wrong_ids}')
+# passports = agregate_people('input.txt')
+# wrong_ids = validate(passports)
+# print(f'Passports:\n {len(passports)}\nWrong ids:\n {len(wrong_ids)}\nList of wrong ids:\n{wrong_ids}')
 
 
+def compare(temp_contener: dict, key: str, value: str, length: int, lower_value: int, upper_value: int) -> dict:
+    if (len(value) == length) and (lower_value >= int(value) <= upper_value):
+        temp_contener['result'].append(f'{key} valid: {value}')
+        temp_contener['counter'] += 1
+    else:
+        temp_contener['result'].append(f'{key} invalid: {value}')
+    return temp_contener
 
 
+def regex_finder(temp_contener: dict, key: str, value: str) -> dict:
+    if key == 'hgt':
+        str_to_compare = re.findall('\d{0,8} ?\w{2}', value)
+    elif key == 'hcl':
+        str_to_compare = re.findall('#\S{6}', value)
+    elif key == 'ecl':
+        str_to_compare = re.findall('blu|amb|brn|gry|hzl|oth', value)
+    elif key == 'pid':
+        str_to_compare = re.findall('0?0?0?0?0?\d{9}', value)
+    elif key == 'cid':
+        str_to_compare = re.findall('(...)', value)
+    else:
+        str_to_compare = []
+
+    if not str_to_compare:
+        temp_contener['result'].append(f'{key} invalid: {value}')
+    elif value == str_to_compare[0]:
+        temp_contener['result'].append(f'{key} valid: {value}')
+        temp_contener['counter'] += 1
+    else:
+        temp_contener['result'].append(f'{key} invalid: {value}')
+
+    return temp_contener
+
+
+def check_correctness(passports: list) -> dict:
+    temp_content = {'result': [], 'counter': 0}
+
+    for passport in passports:
+        for key, value in passport.items():
+            if key == 'byr':
+                temp_content = compare(temp_content, key, value, 4, 1920, 2002)
+            elif key == 'iyr':
+                temp_content = compare(temp_content, key, value, 4, 2010, 2020)
+            elif key == 'eyr':
+                temp_content = compare(temp_content, key, value, 4, 2020, 2030)
+            else:
+                pass
+                # temp_content = regex_finder(temp_content, key, value)
+    return temp_content
+
+
+temp_contener = check_correctness(agregate_people('input.txt'))
+for index in temp_contener['result']:
+    print(index)
+print(temp_contener['counter'])
+
+# print(locals()[key](value) -> # calling function by name
+
+#test_string = '888 409404'
+#result = re.findall('\d{3} ?\d{3} ?\d{3} ?', test_string)
+#print(result)
+#
+## printowanie tylko jak jest wyniki:
+#if result:
+#    print(result)
