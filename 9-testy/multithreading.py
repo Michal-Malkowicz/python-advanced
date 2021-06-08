fd9ae458be44796ab5bd0aed8303537c7b05cb6b
Michal Malkowicz <michal.malkowicz@diehl.com>
2021-06-08 08:40:21 +0200

    parser-advanced part 1 done

--- 9-testy/multithreading.py
+++ 9-testy/multithreading.py
@@ -1,1 +1,20 @@
-import threadingimport
+import logging
+import threading
+import time
+
+message_format = '%(asctime)s: %(message)s'
+logging.basicConfig(format=message_format, level=logging.DEBUG, datefmt='%H:%M:%S')
+
+
+def test_function(argument):
+    logging.info(f'Function{argument} started')
+    time.sleep(1)
+    logging.info(f'Function{argument} started')
+
+
+logging.info('process started')
+my_task1 = threading.Thread(target=test_function, args=(1, ))
+my_task2 = threading.Thread(target=test_function, args=(2, ))
+my_tasks = []
+my_tasks
+#xdist - n- auto - pytestowe uwspółbieżnienie
